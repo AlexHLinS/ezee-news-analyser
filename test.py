a243ad829fe2cc9a81c17a078c70daba3fc651f0
Alex Shkil <alexhlins@gmail.com>
2022-06-13 04:44:30 +0300

    get_published_count(uid: str, similarity_criteria: int)

--- test.py
+++ test.py
@@ -146,5 +146,15 @@ def get_earlest_url_from_uid(uid:str, similarity_criteria):
     earlest = get_earlest_url(relative_urls_with_dates)
     return earlest
 
+def get_published_count(uid: str, similarity_criteria: int):
+    """
+    :param uid: uid: UID результатов анализа на text.ru
+    :param similarity_criteria: процент совпадения, ниже которого результаты не выдаются целое число от 0 до 100
+    :return: кол-во публикаций с совпадением не ниже similarity_criteria
+    """
+    result = len(get_relative_urls(uid, similarity_criteria))
+    return result
 
-print(get_earlest_url_from_uid(test_uid,7))
+
+
+print(get_published_count(test_uid,7))
